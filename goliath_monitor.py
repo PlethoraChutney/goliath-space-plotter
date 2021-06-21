77180b54d4491a1e4d36986ad97555a77cdb9eee
Rich <rich@posert.com>
2021-06-21 16:49:59 -0700

    Works

---
+++ goliath_monitor.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import plotly.express as px
+
+# from https://stackoverflow.com/questions/12523586/python-format-size-application-converting-b-to-kb-mb-gb-tb/63839503
+def humanbytes(B):
+   'Return the given bytes as a human friendly KB, MB, GB, or TB string'
+   B = float(B)
+   KB = float(1024)
+   MB = float(KB ** 2) # 1,048,576
+   GB = float(KB ** 3) # 1,073,741,824
+   TB = float(KB ** 4) # 1,099,511,627,776
+
+   if B < KB:
+      return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
+   elif KB <= B < MB:
+      return '{0:.2f} KB'.format(B/KB)
+   elif MB <= B < GB:
+      return '{0:.2f} MB'.format(B/MB)
+   elif GB <= B < TB:
+      return '{0:.2f} GB'.format(B/GB)
+   elif TB <= B:
+      return '{0:.2f} TB'.format(B/TB)
+
+def read_table(filename):
+    table = pd.read_table(
+        filename,
+        delim_whitespace = True,
+        header = 0,
+        names = ['Size', 'Path']
+    )
+
+    split_paths = table.Path.str.split(pat = '/', expand = True).drop(columns = [0,1])
+    split_paths.columns = ['Location', 'Lab', 'User', 'Dir']
+    split_paths['Size'] = table['Size']
+    split_paths = split_paths[split_paths.Dir.notnull()]
+    split_paths["Readable"] = split_paths.Size.apply(humanbytes)
+    print(split_paths)
+
+    return(split_paths)
+
+def make_plot(table):
+    fig = px.sunburst(
+        table,
+        path = ['Location', 'Lab', 'User', 'Dir'],
+        values = 'Size',
+        color = 'User',
+        hover_data = ['Readable']
+        )
+    fig.show()
+
+if __name__ == '__main__':
+    table = read_table('test-du.txt')
+    make_plot(table)
